6dd14e30ef3670c85623027677df070eef07deaa
Preston <childrep@vt.edu>
2022-05-30 16:02:52 -0400

    Start of spleeter

--- base.py
+++ base.py
@@ -1,40 +1,70 @@
 from playsound import playsound
 from urllib.parse import urlparse
-from youtube_search import YoutubeSearch
-import youtube_dl
-import webrtcvad
+from yt_dlp import YoutubeDL
+from youtubesearchpython import VideosSearch
+from spleeter.separator import Separator
+import os
 
-vad = webrtcvad.Vad(3)
+separator = Separator('spleeter:2stems')
+SONGS_DIR = 'songs/'
 
 
 def is_url(item):
     return bool(urlparse(item).scheme)
 
 
-def fetch_video(item):
+def get_title(item):
+    if item[-4:] == 'song':
+        return item[:-5]
+    else:
+        return item
+
+
+def get_song_fp(item):
+    return SONGS_DIR + get_title(song) + '.mp3'
+
+
+def fetch_song(item, force=False):
+    search = VideosSearch(item, limit=1).result()['result'][0]
     if is_url(item):
+        # noinspection PyTypeChecker
+        title = search['title']
         to_fetch = item
     else:
-        to_fetch = 'https://www.youtube.com' + YoutubeSearch(item, max_results=1).videos[0]['link']
+        title = get_title(item)
+        # noinspection PyTypeChecker
+        to_fetch = search['link']
 
-    ydl_opts = {
+    file_path = get_song_fp(title)
+
+    dl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': item + '.%(ext)s',
+        'outtmpl': file_path,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([to_fetch])
+
+    if force or not os.path.exists(file_path):
+        YoutubeDL(dl_opts).extract_info(to_fetch)
+    else:
+        print('Using saved version of song:  ', title)
+
+
+def delete_song(name):
+    os.remove(get_song_fp(name))
 
 
 def process_song(item):
     print(item)
+    separator.separate_to_file(get_song_fp(item), 'splits/')
 
 
 if __name__ == '__main__':
-    song = 'caught a ghost'
-    fetch_video(song)
-    # playsound(song + '.mp3')
+    song = 'culture of fear song'
+    print(get_song_fp(song))
+    #fetch_song(song)
+    #process_song(song)
+    #playsound(get_song_fp(song))
